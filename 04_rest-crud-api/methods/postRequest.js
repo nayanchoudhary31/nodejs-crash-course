f49f01c7fb51da7d9c3fee5f8b11f19bf0647ef9
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-05 23:08:46 +0530

    added-post-method

--- 04_rest-crud-api/methods/postRequest.js
+++ 04_rest-crud-api/methods/postRequest.js
@@ -1,3 +1,26 @@
-module.exports = (req,resp)=>{
-    
+const crypto = require('crypto');
+const bodyparser = require('../utils/body-parser');
+const writeFile = require('../utils/write-file');
+module.exports = async (req, resp) => {
+
+    if (req.url === "/api/movies/") {
+        try {
+            let body = await bodyparser(req);
+            body.id = crypto.randomUUID();
+            req.movies.push(body);
+            writeFile(req.movies)
+            resp.writeHead(201, { "Content-Type": "application/json" });
+            resp.end();
+
+        } catch (error) {
+            console.log(error);
+            resp.writeHead(400, { "Content-Type": "application/json" });
+            resp.end(JSON.stringify({ title: "Validation Failed", message: "Request body in not valid" }))
+        }
+
+    } else {
+        resp.writeHead(404, { "Content-Type": "application/json" });
+        resp.end(JSON.stringify({ title: "Not Found", message: "Route Not Found" }))
+    }
+
 }
