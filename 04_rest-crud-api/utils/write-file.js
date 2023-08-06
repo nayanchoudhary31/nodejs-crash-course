4dec00a1e61737449705e705f0ef75f6a95fd5c8
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-06 23:12:26 +0530

    correct-path

--- 04_rest-crud-api/utils/write-file.js
+++ 04_rest-crud-api/utils/write-file.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 module.exports = (data) => {
     try {
-        fs.writeFileSync(path.join(__dirname, "..", "data", "movies.json"), JSON.stringify(data), 'utf-8')
+        fs.writeFileSync(path.join(__dirname, "..", "data", "movie.json"), JSON.stringify(data), 'utf-8')
     } catch (error) {
         console.log(error);
     }
