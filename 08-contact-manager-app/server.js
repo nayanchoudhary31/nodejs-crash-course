65709425e0c62c4956c5a1b155d06521320836e2
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-10 19:30:37 +0530

    added user-routes

--- 08-contact-manager-app/server.js
+++ 08-contact-manager-app/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config()
 const app = express();
 const contactRoutes = require('./routes/contactsRoutes');
+const userRoutes = require('./routes/userRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const connectDb = require('./config/dbConnect');
 const PORT = process.env.PORT || 5000
@@ -10,6 +11,7 @@ connectDb();
 app.use(express.json());
 
 app.use("/api/contacts", contactRoutes);
+app.use("/api/user", userRoutes)
 app.use(errorHandler)
 
 app.listen(PORT, () => {
