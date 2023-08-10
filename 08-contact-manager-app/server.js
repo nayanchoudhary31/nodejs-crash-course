2fa4a83fc48e2df0aefe15c8604dc5f7bd92ccfb
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-10 15:18:59 +0530

    added db connection

--- 08-contact-manager-app/server.js
+++ 08-contact-manager-app/server.js
@@ -2,14 +2,16 @@ const express = require('express');
 require('dotenv').config()
 const app = express();
 const contactRoutes = require('./routes/contactsRoutes');
-const errorHandler = require('./middleware/errorHandler')
+const errorHandler = require('./middleware/errorHandler');
+const connectDb = require('./config/dbConnect');
 const PORT = process.env.PORT || 5000
 
+connectDb();
 app.use(express.json());
 
-app.use("/api/contacts",contactRoutes);
+app.use("/api/contacts", contactRoutes);
 app.use(errorHandler)
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port : ${PORT}`);
 })
