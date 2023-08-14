fe8737a3f7466e9b7ab048064a0de50009d0b053
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-14 14:16:53 +0530

    updated userRoutes

--- 08-contact-manager-app/routes/userRoutes.js
+++ 08-contact-manager-app/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { registerUser, loginUser, getCurrentUser } = require('../controller/userController');
+const validateToken = require('../middleware/validationHandler');
 const router = express.Router();
 
 router.post("/register", registerUser)
 
 router.post("/login", loginUser)
 
-router.get("/current", getCurrentUser)
+router.get("/current", validateToken, getCurrentUser)
 
 module.exports = router;
