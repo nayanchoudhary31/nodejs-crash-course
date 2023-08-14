328d202df7136e624be6740993ec9df3003db04f
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-08-14 14:14:46 +0530

    updated-private-routes

--- 08-contact-manager-app/routes/contactsRoutes.js
+++ 08-contact-manager-app/routes/contactsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getContact, getContactById, updateContactById, deleteContactById, createContact } = require("../controller/contactController")
+const { getContact, getContactById, updateContactById, deleteContactById, createContact } = require("../controller/contactController");
+const validateToken = require('../middleware/validationHandler');
 
 
 
-
+router.use(validateToken);
 router.route("/").get(getContact).post(createContact)
 
 router.route("/:id").get(getContactById).put(updateContactById).delete(deleteContactById)
